005871cb1bf82d1d9885d916b601ea85836afd32
Matthew <mattchu18@gmail.com>
2023-05-04 19:54:49 -0700

    creating LoginFormPage component

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,12 +6,6 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-
 // Static routes
 // Serve React build files in production
 if (process.env.NODE_ENV === 'production') {
@@ -53,10 +47,5 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-
-
-
-
-
 
 module.exports = router;
