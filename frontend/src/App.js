e45fb2d2d8ba03926f88f20f5511ab7cc595b5de
Matthew <mattchu18@gmail.com>
2023-05-05 10:43:43 -0700

    created Navigation folder and navigation component function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,7 +15,10 @@ function App() {
   }, [dispatch]);
 
   return (
-    isLoaded && (
+    <>
+
+    <Navigation isLoaded={isLoaded} />
+    {isLoaded && (
       <Switch>
         <Route path="/login">
           <LoginFormPage />
@@ -23,7 +27,8 @@ function App() {
           <SignupFormPage />
         </Route>
       </Switch>
-    )
+    )}
+    </>
   );
 }
 
