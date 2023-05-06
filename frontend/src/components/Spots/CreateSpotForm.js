c8d63d8d103d7133f8be9e88cc81c84cb657c958
Matthew <mattchu18@gmail.com>
2023-05-06 16:39:07 -0700

    created CreateSpotForm component

--- frontend/src/components/Spots/CreateSpotForm.js
+++ frontend/src/components/Spots/CreateSpotForm.js
@@ -1,1 +1,24 @@
 import SpotForm from './SpotForm'
+
+const CreateSpotForm = () => {
+    const spot = {
+        address: '',
+        city: '',
+        state: '',
+        country: '',
+        name: '',
+        description: '',
+        price: '',
+        previewImage: '',
+        imageUrl: ''
+    }
+
+    return (
+        <SpotForm
+            spot={spot}
+            formType="Create Spot"
+        />
+    )
+}
+
+export default CreateSpotForm;
