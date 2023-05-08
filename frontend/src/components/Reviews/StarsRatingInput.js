a8a1c0c4d7e04aefd164cb323ffaa5f3233cd9c6
Matthew <mattchu18@gmail.com>
2023-05-08 15:42:01 -0700

    formatted

--- frontend/src/components/Reviews/StarsRatingInput.js
+++ frontend/src/components/Reviews/StarsRatingInput.js
@@ -2,50 +2,52 @@ import { useState, useEffect } from "react"
 import "./StarsRatingInput.css"
 
 const StarsRatingInput = ({ stars, disabled, onChange }) => {
-  const [activeStars, setActiveStars] = useState(stars)
-  useEffect (()=> {
-    setActiveStars(stars)
+    const [activeStars, setActiveStars] = useState(stars)
 
-  },[stars] )
-console.log("onChange: ", onChange)
+    useEffect(() => {
+        setActiveStars(stars)
 
-//can refactor this to an array method or forLoop
-  return (
-    <>
-      <div className="stars-input">
-        <div onMouseEnter={(e) => disabled === false ? setActiveStars(1) : null}
-          onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
-          className={(activeStars >= 1 ? 'filled' : 'empty')}
-          onClick={(e) => disabled === false ? onChange(1) : null}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={(e) => disabled === false ? setActiveStars(2) : null}
-          onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
-          className={(activeStars >= 2 ? 'filled' : 'empty')}
-          onClick={() => disabled === false ? onChange(2) : null}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={(e) => disabled === false ? setActiveStars(3) : null}
-          onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
-          className={(activeStars >= 3 ? 'filled' : 'empty')}
-          onClick={() => disabled === false ? onChange(3) : null}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={(e) => disabled === false ? setActiveStars(4) : null}
-          onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
-          className={(activeStars >= 4 ? 'filled' : 'empty')}
-          onClick={() => disabled === false ? onChange(4) : null}>
-          <i className="fa fa-paw"></i>
-        </div>
-        <div onMouseEnter={(e) => disabled === false ? setActiveStars(5) : null}
-          onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
-          className={(activeStars >= 5 ? 'filled' : 'empty')}
-          onClick={() => disabled === false ? onChange(5) : null}>
-          <i className="fa fa-paw"></i>
-        </div>
-      </div>
-    </>
-  );
+    }, [stars])
+
+    console.log("onChange: ", onChange)
+
+    //can refactor this to an array method or forLoop
+    return (
+        <>
+            <div className="stars-input">
+                <div onMouseEnter={(e) => disabled === false ? setActiveStars(1) : null}
+                    onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
+                    className={(activeStars >= 1 ? 'filled' : 'empty')}
+                    onClick={(e) => disabled === false ? onChange(1) : null}>
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div onMouseEnter={(e) => disabled === false ? setActiveStars(2) : null}
+                    onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
+                    className={(activeStars >= 2 ? 'filled' : 'empty')}
+                    onClick={() => disabled === false ? onChange(2) : null}>
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div onMouseEnter={(e) => disabled === false ? setActiveStars(3) : null}
+                    onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
+                    className={(activeStars >= 3 ? 'filled' : 'empty')}
+                    onClick={() => disabled === false ? onChange(3) : null}>
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div onMouseEnter={(e) => disabled === false ? setActiveStars(4) : null}
+                    onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
+                    className={(activeStars >= 4 ? 'filled' : 'empty')}
+                    onClick={() => disabled === false ? onChange(4) : null}>
+                    <i className="fa fa-paw"></i>
+                </div>
+                <div onMouseEnter={(e) => disabled === false ? setActiveStars(5) : null}
+                    onMouseLeave={(e) => disabled === false ? setActiveStars(stars) : null}
+                    className={(activeStars >= 5 ? 'filled' : 'empty')}
+                    onClick={() => disabled === false ? onChange(5) : null}>
+                    <i className="fa fa-paw"></i>
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default StarsRatingInput;
