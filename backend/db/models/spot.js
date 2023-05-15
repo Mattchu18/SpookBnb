429e53c492685709d33e98b550fa0ff18311a441
Matthew <mattchu18@gmail.com>
2023-05-14 22:25:26 -0700

    altered backend to accomodate text instead of string

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -68,7 +68,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
     },
     description: {
-      type: DataTypes.STRING,
+      type: DataTypes.TEXT,
     },
     price: {
       type: DataTypes.DECIMAL
--- backend/db/models/spotimage.js
+++ backend/db/models/spotimage.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
   }
   SpotImage.init({
     spotId: DataTypes.INTEGER,
-    url: DataTypes.STRING,
+    url: DataTypes.TEXT,
     preview: DataTypes.BOOLEAN
   }, {
     sequelize,
